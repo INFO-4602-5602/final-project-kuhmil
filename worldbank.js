337c7143f7dbcca1808cc6310e3a0ef55b904777
kuhmil <caow7208@colorado.edu>
2017-05-08 17:13:31 -0600

    update

---
+++ worldbank.js
@@ -0,0 +1,23 @@
+var countries = [
+  {
+    "Country": "Austria",
+    "CountryCode": "AFG",
+    "Region": "Asia | South & West",
+    "Continent": "Asia",
+    "GDP": 142,
+    "GDP_perCapita": 205,
+    "lifeExpectancy": 59.60009756
+  },
+  {
+    "Country": "Albania",
+    "CountryCode": "ALB",
+    "Region": "Europe | South & East",
+    "Continent": "Europe",
+    "GDP": 11926957254.63,
+    "GDP_perCapita": 4094.36,
+    "lifeExpectancy": 76.9785122
+  }
+];
+
+
+ 
